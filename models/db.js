0d50237736f86f3870568e5dce0cde355f8e70de
Shivam Upadhyay <shivamup4545@gmail.com>
2021-04-26 22:33:51 +0530

    api

--- models/db.js
+++ models/db.js
@@ -3,20 +3,9 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-// mongoose.connect(process.env.DB_URL , {useCreateIndex:true , useFindAndModify:false , useNewUrlParser:true , useUnifiedTopology:true})
-// .then(()=>{
-//     console.log("Database Connected")
-// }).catch((err)=>{
-//     console.log(err)
-// })
-
-mongoose.connect(
-    process.env.DB_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    function (err, res) {
-        try {
-            console.log('Connected to Database');
-        } catch (err) {
-            throw err;
-        }
-    });
+mongoose.connect(process.env.DB_URL , {useCreateIndex:true , useFindAndModify:false , useNewUrlParser:true , useUnifiedTopology:true})
+.then(()=>{
+    console.log("Database Connected")
+}).catch((err)=>{
+    console.log(err)
+})
