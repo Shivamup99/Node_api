2b19ddb658a401f8eb6a5cc38a7b678171de3d5c
Shivam Upadhyay <shivamup4545@gmail.com>
2021-04-26 22:20:44 +0530

    api

--- server.js
+++ server.js
@@ -42,12 +42,12 @@ app.use(express.static(__dirname + '/public'));
  //}
  //app.use(errHandler)
 
- if(process.env.NODE_ENV=="production"){
-     app.use(express.static("client/build"))
-      app.get("/*",(req,res)=>{
-          res.sendFile(path.resolve(__dirname ,'client','build','index.html'))
-      })
- }
+//  if(process.env.NODE_ENV=="production"){
+//      app.use(express.static("client/build"))
+//       app.get("/*",(req,res)=>{
+//           res.sendFile(path.resolve(__dirname ,'client','build','index.html'))
+//       })
+//  }
 
 
 
